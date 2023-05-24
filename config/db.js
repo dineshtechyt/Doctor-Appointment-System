ddc8dd1aa42d6201be19b0c7945d4e08d121daea
dinesh pawar <pawardinesh8668@gmail.com>
2023-05-24 13:04:39 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://0.0.0.0:27017/doctor_appointment");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log(`mongodb connected ${mongoose.connection.host}`);
   } catch (error) {
     console.log(error);
